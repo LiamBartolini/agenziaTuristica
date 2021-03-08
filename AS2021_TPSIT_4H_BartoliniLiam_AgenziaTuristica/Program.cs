f29ae6f595ac439f768099fbcfeb787a5c9035ea
LiamBartolini <liambartolini@gmail.com>
2021-03-08 15:21:33 +0100

    first commit

---
+++ AS2021_TPSIT_4H_BartoliniLiam_AgenziaTuristica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AS2021_TPSIT_4H_BartoliniLiam_AgenziaTuristica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
